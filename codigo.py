5de0764ad8147323fe77ab063f1158bdb11cbe1a
Ricard0 R Z <ricardoramirezzarate50@gmail.com>
2025-08-04 16:35:02 -0600

    subiendo el archivo de python

---
+++ codigo.py
@@ -0,0 +1,10 @@
+nombre = "Lucas"
+apellido = "Dalto"
+
+print (f"Hola, {nombre} {apellido}" +" como estas?")
+saludo = "Hola, {nombre} {apellido}"
+print(saludo)
+html = f"<h1>Hola, {nombre} {apellido}</h1>"print(saludo.format(nombre=nombre, apellido=apellido))
+print(saludo.format(nombre=nombre.upper(), apellido=apellido.lower()))
+print(saludo.format(nombre=nombre.capitalize(), apellido=apellido.capitalize()))
+
