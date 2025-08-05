1f7c0f8688abee7518d8e2a974106717be4d2c74
Ricardo RZ <ricardoramirezzarate50@gmail.com>
2025-08-04 18:38:25 -0600

    solo subelo plis

--- codigo.py
+++ codigo.py
@@ -4,7 +4,7 @@ apellido = "Dalto"
 print (f"Hola, {nombre} {apellido}" +" como estas?")
 saludo = "Hola, {nombre} {apellido}"
 print(saludo)
-html = f"<h1>Hola, {nombre} {apellido}</h1>"print(saludo.format(nombre=nombre, apellido=apellido))
+html = f"<h1>Hola, {nombre} {apellido}</h1>" (saludo.format(nombre=nombre, apellido=apellido))
 print(saludo.format(nombre=nombre.upper(), apellido=apellido.lower()))
 print(saludo.format(nombre=nombre.capitalize(), apellido=apellido.capitalize()))
 
